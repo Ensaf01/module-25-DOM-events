da87dd3a3e3a6f674533613e00c591b0275eafbd
ensaf01 <mohammedensaf402@gmail.com>
2026-07-19 00:17:50 +0600

    text vlaue updated and text vlaue updated by input value

---
+++ event/event-summary.js
@@ -0,0 +1,28 @@
+
+
+// option -1
+
+function statusChange(){
+    const statusChangeButton=document.getElementById('status-change');
+    statusChangeButton.innerText='updated the staus';
+}
+
+// option 2
+
+document.getElementById('statusChange_2ND').addEventListener('click', function(){
+    const statusChange_2nd=document.getElementById('status-change');
+    statusChange_2nd.innerText='this is changing by 2nd option ';
+})
+
+
+// 2nd work 
+
+document.getElementById('btn-updated').addEventListener('click',function(){
+    const inputFiled=document.getElementById('input-filed');
+    const inputValue=inputFiled.value; // id diye oi vlaue take store korlam
+
+    const message=document.getElementById('ensaf');
+    message.innerText=inputValue;
+    inputFiled.value='';// auto refresh by empty 
+    //inputFiled.value='change by button';
+})
